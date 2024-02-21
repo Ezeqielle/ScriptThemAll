bb8c4babccb3fc285e043a1259c15cc0b66c49c4
Mathis DI MASCIO <mathis.dimascio@gmail.com>
2024-02-21 15:58:48 +0100

    init branch

---
+++ monitoring/prometheus.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Name: prometheus.sh
+# Description: This will install and setup prometheus
+# Author: Ezeqielle
+# Version: 1.0.0
+# Last updated: 2024-02-20
+# Usage: sudo ./prometheus.sh
